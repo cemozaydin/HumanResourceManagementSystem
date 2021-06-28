25b9038f9179168cfd3c07fd11e0ba3f2f63c6f9
cemozaydin <cem.ozaydin@gmail.com>
2021-06-28 22:59:06 +0300

    28.06.2021 - Code refactoring

--- src/main/java/kodlamaio/hrms/api/controllers/WorkplaceTypesController.java
+++ src/main/java/kodlamaio/hrms/api/controllers/WorkplaceTypesController.java
@@ -3,12 +3,14 @@ package kodlamaio.hrms.api.controllers;
 import kodlamaio.hrms.business.abstracts.WorkplaceTypeService;
 import kodlamaio.hrms.core.utilities.results.Result;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/workplacetypes")
+@CrossOrigin
 public class WorkplaceTypesController {
     private WorkplaceTypeService workplaceTypeService;
 
